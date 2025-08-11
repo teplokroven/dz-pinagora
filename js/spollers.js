482b480e8e9314b18c5a53f376181b1964dc9f15
teplokroven <chemnkss@gmail.com>
2025-08-11 18:32:55 +0300

    обновление правок

--- js/spollers.js
+++ js/spollers.js
@@ -1,63 +1,60 @@
 class Spollers {
-  constructor(wrapClass, multiOpen = false) {
-    this.spollersWrap = document.querySelector(`.${wrapClass}`);
-    this.spollers = this.spollersWrap.querySelectorAll('.spoller');
-    this.activeSpollers = this.spollersWrap.querySelectorAll('.spoller[open]');
-    this.multiOpen = multiOpen == 'multiOpen' ? true : false;
-    this.events();
+	constructor(wrapClass, multiOpen = false) {
+		this.spollersWrap = document.querySelector(`.${wrapClass}`);
+		this.spollers = this.spollersWrap.querySelectorAll('.spoller');
+		this.activeSpollers = this.spollersWrap.querySelectorAll('.spoller[open]');
+		this.multiOpen = multiOpen == 'multiOpen' ? true : false;
+		this.events();
 
-    if (this.activeSpollers) {
-      this.activeSpollers.forEach((spoller) => {
-        this.openSpoller(spoller);
-      });
-    }
-  }
+		if (this.activeSpollers) {
+			this.activeSpollers.forEach((spoller) => {
+				this.openSpoller(spoller);
+			});
+		}
+	}
 
-  events() {
-    this.spollers.forEach((spoller) => {
-      spoller.addEventListener('click', (event) => {
-        event.preventDefault();
-        const isHeaderClick = event.target.closest('.spoller__header');
-        if (!isHeaderClick) return;
+	events() {
+		this.spollers.forEach((spoller) => {
+			spoller.addEventListener('click', (event) => {
+				event.preventDefault();
+				const isHeaderClick = event.target.closest('.spoller__header');
+				if (!isHeaderClick) return;
 
-        if (spoller.open) {
-          this.closeSpoller(spoller);
-        } else {
-          this.openSpoller(spoller);
-        }
-      });
-    });
-  }
+				if (spoller.open) {
+					this.closeSpoller(spoller);
+				} else {
+					this.openSpoller(spoller);
+				}
+			});
+		});
+	}
 
-  openSpoller(spoller) {
-    const spollerContent = spoller.querySelector('.spoller__content');
-    const titleColor = spoller.querySelector('.spoller__title');
+	openSpoller(spoller) {
+		const spollerContent = spoller.querySelector('.spoller__content');
 
-    spoller.setAttribute('open', '');
-    titleColor.style.color = '#0a8ed9';
-    spollerContent.style.maxHeight = '0';
-    requestAnimationFrame(() => {
-      spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
-    });
+		spoller.classList.add('open');
+		spoller.setAttribute('open', '');
+		spollerContent.style.maxHeight = '0';
+		requestAnimationFrame(() => {
+			spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
+		});
 
-    if (this.activeSpoller && this.activeSpoller !== spoller && !this.multiOpen) {
-      this.closeSpoller(this.activeSpoller);
-    }
-    this.activeSpoller = spoller;
-  }
+		if (this.activeSpoller && this.activeSpoller !== spoller && !this.multiOpen) {
+			this.closeSpoller(this.activeSpoller);
+		}
+		this.activeSpoller = spoller;
+	}
 
-  closeSpoller(spoller) {
-    const spollerContent = spoller.querySelector('.spoller__content');
-    const titleColor = spoller.querySelector('.spoller__title');
+	closeSpoller(spoller) {
+		const spollerContent = spoller.querySelector('.spoller__content');
 
-    spoller.classList.remove('open');
-    titleColor.style.color = '#4f4f4f';
-    spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
-    requestAnimationFrame(() => {
-      spollerContent.style.maxHeight = '0';
-    });
-    setTimeout(() => spoller.removeAttribute('open'), 300);
-  }
+		spoller.classList.remove('open');
+		spollerContent.style.maxHeight = spollerContent.scrollHeight + 'px';
+		requestAnimationFrame(() => {
+			spollerContent.style.maxHeight = '0';
+		});
+		setTimeout(() => spoller.removeAttribute('open'), 300);
+	}
 }
 
 new Spollers('spollers');
